078b701d77e5d68c73a6575c213a67fb0054184d
Andrii <gunter0104@gmail.com>
2018-04-26 17:30:40 +0300

    test
    test

--- src/com/company/Registration.java
+++ src/com/company/Registration.java
@@ -1,1 +1,29 @@
-package com.company;import java.util.Scanner;import static com.company.Serialization.serialize;public class Registration {    public static String newUser() {        User user = new User();        Scanner scan = new Scanner(System.in);        System.out.println("Enter Your Name");        user.setUsername(scan.nextLine());        System.out.println("Enter password");        user.setPassword(scan.nextLine());        System.out.println("Confirm password");        user.setConfirmPassword(scan.nextLine());        if (user.getPassword().equals(user.getConfirmPassword())) {            System.out.println("you are registered " + user.getUsername());            return serialize(user);        } else {            System.out.println("retard");        }        return null;    }}
+package com.company;
+
+import java.util.Scanner;
+
+import static com.company.Serialization.serialize;
+
+public class Registration {
+
+    public static String newUser() {
+        User user = new User();
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter Your Name");
+        user.setUsername(scan.nextLine());
+        System.out.println("Enter password");
+        user.setPassword(scan.nextLine());
+        System.out.println("Confirm password");
+        user.setConfirmPassword(scan.nextLine());
+
+        if (user.getPassword().equals(user.getConfirmPassword())) {
+            System.out.println("you are registered " + user.getUsername());
+            return serialize(user);
+        } else {
+            System.out.println("retard");
+        }
+        return null;
+    }
+}
+
